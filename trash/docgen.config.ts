0f0ffa4ca3a85b369a2427626531deffc1ed94be
Okpainmo <okpainmoandrew@gmail.com>
2025-08-07 10:08:16 +0100

    chore: trashed content, and addition of contracts architecuture diagram

---
+++ trash/docgen.config.ts
@@ -0,0 +1,13 @@
+// import type { Config } from "solidity-docgen";
+
+// const config: Config = {
+//     input: "./contracts",
+//     output: "./docs",
+//     compiler: {
+//         version: "0.8.20",
+//     },
+//     pages: "files",
+//     sourcesDir: "contracts",
+// };
+
+// export default config;
