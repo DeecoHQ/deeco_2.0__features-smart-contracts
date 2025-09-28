24a69ab7205264681b58d637d95ed3ab614d742a
Okpainmo <okpainmoandrew@gmail.com>
2025-09-28 13:24:08 +0100

    chore: contract(deployed) on-chain addresses

---
+++ deployedContracts.ts
@@ -0,0 +1,14 @@
+export const deployedContracts = [
+    {
+        contractName: "Admin Management",
+        Address: "0x342D864A17135690Def25b488099dB986fA369d0",
+    },
+    {
+        contractName: "Order Management",
+        Address: "0xa1eea08ec65e3869091F0998cfc0c2E8dCa048f2",
+    },
+    {
+        contractName: "Product Management",
+        Address: "0x73746FEEf4379ca1a23Fb83d7B1E6999B083f326",
+    },
+];
